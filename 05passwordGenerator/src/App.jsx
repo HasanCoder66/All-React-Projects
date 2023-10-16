bb8b6d18c9030f6c3bac35b9b1b71efcaf84bb3a
66CoderHasan <hasanashraf874@gmail.com>
2023-10-15 23:22:55 -0700

    05 project continue

---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='text-white '>Password Generator</h1>
+    <div  className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700'>
+test
+    </div>
+    </>
+  )
+}
+
+export default App
