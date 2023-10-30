e5ec49d61e0d049347dfbc3a190a56528b24fab5
66CoderHasan <hasanashraf874@gmail.com>
2023-10-30 19:30:28 +0500

    09 themeSwitcher Project

--- 09themeSwitcher/src/App.jsx
+++ 09themeSwitcher/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
-import "./App.css";
+// import "./App.css";
 import { ThemeProvider } from "./context/theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
 
 function App() {
   const [themeMode ,setThemeMode] = useState('light')
@@ -24,8 +26,10 @@ function App() {
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
-{/* asdf */}
-          <div className="w-full max-w-sm mx-auto"></div>
+<ThemeBtn/>
+          <div className="w-full max-w-sm mx-auto">
+            <Card/>
+          </div>
         </div>
       </div>
     </ThemeProvider>
