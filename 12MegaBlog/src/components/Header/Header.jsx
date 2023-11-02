7394f17f98e18a44eec058c8e62a44e7ce8ac9dc
66CoderHasan <hasanashraf874@gmail.com>
2023-11-02 08:35:01 +0500

    header , footer , logout btn work

--- 12MegaBlog/src/components/Header/Header.jsx
+++ 12MegaBlog/src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import { Container , Logo , LogoutBtn , } from '../index.js'
+import { Link , useNavigate } from 'react-router-dom'
+import { UseSelector } from 'react-redux'
 function Header() {
   return (
     <div>Header</div>
--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,7 +1,7 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import LogoutBtn from "./Header/Logout";
+import Logo from "./Logo";
+import Container from "./Container/Container"; 
 
-export {
-    Header,
-    Footer
-}
+export { Header, Footer, LogoutBtn, Logo, Container };
