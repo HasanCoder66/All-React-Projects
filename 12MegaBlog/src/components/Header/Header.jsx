7fb6c47304f09b4966524a0bb4b07ed140e91bab
66CoderHasan <hasanashraf874@gmail.com>
2023-11-02 18:15:59 +0500

    header in process

--- 12MegaBlog/src/components/Header/Header.jsx
+++ 12MegaBlog/src/components/Header/Header.jsx
@@ -3,8 +3,54 @@ import { Container , Logo , LogoutBtn , } from '../index.js'
 import { Link , useNavigate } from 'react-router-dom'
 import { UseSelector } from 'react-redux'
 function Header() {
+  const authStatus = UseSelector((state) => state.auth.status)
+  const navigate = useNavigate()
+  const navItems = [
+    {
+      name: 'Home',
+      slug: "/",
+      active: true
+    }, 
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+  },
+  {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+  },
+  {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+  },
+  {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+  },
+  ]
   return (
-    <div>Header</div>
+    <header className='py-3 shadow bg-gray500-'>
+      <Container>
+    <nav className='flex'>
+      <div className='mr-4'>
+        <Link to={"/"}>
+        <Logo widht='70px'/>
+        </Link>
+      </div>
+      <ul className='flex ml-auto'>
+            {navItems.map((item) => item.active ? (
+              <li key={item.name}>
+                <button></button>
+              </li>
+            ) : null)}
+          </ul>
+    </nav>
+      </Container>
+    </header>
   )
 }
 
